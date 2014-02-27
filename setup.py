673d9118be7e82658b0538d2fb23b035bbf3b9f3
Jakub Dorňák <jdornak@redhat.com>
2014-02-27 01:37:13 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@
 from setuptools import setup
 
 setup(
-    name         = 'flock',
-    version      = '0.1',
-    description  = 'Flock object uses fcntl.flock to lock (resp. unlock) '
-                   'file descriptor (fd) with operation (op) '
-                   'when entering (resp. leaving) runtime context related to it.',
-    author       = 'Jakub Dorňák',
-    author_email = 'jdornak@redhat.com',
-    url          = 'https://github.com/misli/python-flock',
-    py_modules   = ['flock'],
+    name              = 'flock',
+    version           = '0.1',
+    description       = 'Flock object for with statement ',
+    author            = 'Jakub Dorňák',
+    author_email      = 'jdornak@redhat.com',
+    url               = 'https://github.com/misli/python-flock',
+    py_modules        = ['flock'],
+    long_description  = 'Flock uses fcntl.flock to lock (resp. unlock) '
+                        'file descriptor (fd) with operation (op) '
+                        'when entering (resp. leaving) runtime context related to it.'
 )
